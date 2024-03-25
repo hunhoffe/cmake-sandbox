395469876c170f90b0c755269a74de06dcd9e7a4
Erika Hunhoff <hunhoff.erika@gmail.com>
2024-03-25 01:10:27 -0600

    Add cxxopts

--- README.md
+++ README.md
@@ -9,4 +9,9 @@ sudo dnf install -y boost boost-devel
 sudo dnf install -y double-conversion double-conversion-devel
 sudo dnf install -y gflags gflags-devel glog glog-devel
 sudo dnf install -y folly-devel
+
+mkdir third_party
+cd third_party
+git clone --depth 1 --branch v3.2.0 https://github.com/jarro2783/cxxopts
+
 ```
--- hello.cc
+++ hello.cc
@@ -1,7 +1,9 @@
 #include <iostream>
 #include "folly/MPMCQueue.h"
+#include <cxxopts.hpp>
 
 int main(int argc, char* argv[]) {
     folly::MPMCQueue<uint> *q = new folly::MPMCQueue<uint>(10);
+    cxxopts::Options options("embarcadero", "a totally ordered pub/sub system with CXL");
     std::cout << "Hello!" << std::endl;
 }
