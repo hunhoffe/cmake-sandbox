127d6ba13b354ae8e551ff64243f2abce8a86f72
Erika Hunhoff <hunhoff.erika@gmail.com>
2024-03-25 02:22:08 -0600

    gRPC doesn't work yet

--- README.md
+++ README.md
@@ -1,17 +1,1 @@
 # cmake-sandbox
-
-Setup:
-```
-sudo dnf install -y python-devel
-sudo dnf install -y libevent libevent-devel
-sudo dnf install -y fmt fmt-devel
-sudo dnf install -y boost boost-devel
-sudo dnf install -y double-conversion double-conversion-devel
-sudo dnf install -y gflags gflags-devel glog glog-devel
-sudo dnf install -y folly-devel
-
-mkdir third_party
-cd third_party
-git clone --depth 1 --branch v3.2.0 https://github.com/jarro2783/cxxopts
-git clone --depth 1 --branch 20240116.1 https://github.com/abseil/abseil-cpp.git
-```
---
+++ setup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+rm -rf build
+rm -rf third_party
+
+sudo dnf install -y python-devel
+sudo dnf install -y libevent libevent-devel
+sudo dnf install -y fmt fmt-devel
+sudo dnf install -y boost boost-devel
+sudo dnf install -y double-conversion double-conversion-devel
+sudo dnf install -y gflags gflags-devel glog glog-devel
+sudo dnf install -y folly-devel
+
+# for grpc
+sudo dnf install -y systemd-devel
+#sudo dnf install -y protobuf-devel protobuf-lite-devel
+
+mkdir third_party
+cd third_party
+git clone --depth 1 --branch v3.2.0 https://github.com/jarro2783/cxxopts
+git clone --depth 1 --branch 20240116.1 https://github.com/abseil/abseil-cpp.git
+#git clone --recurse-submodules -b v1.62.0 --depth 1 --shallow-submodules https://github.com/grpc/grpc
